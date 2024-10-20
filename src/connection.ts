ab934e3249ef5522294a48fa8980ed25efd43791
carneirodee <carneirodeyse0@gmail.com>
2024-10-20 09:54:57 -0300

    ADD

--- src/connection.ts
+++ src/connection.ts
@@ -10,30 +10,30 @@ const InitDB = async () => {
   const isTest = process.env.NODE_ENV === 'test';
   const isProduction= process.env.NODE_ENV === 'production';
 
-  if (!isTest || !isProduction) {
+  // if (!isTest || !isProduction) {
 
-    const client = new pg.Client({
-      host: process.env.DB_HOST,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      port: 5432,
-    });
+  //   const client = new pg.Client({
+  //     host: process.env.DB_HOST,
+  //     user: process.env.DB_USER,
+  //     password: process.env.DB_PASSWORD,
+  //     port: 5432,
+  //   });
 
-    await client.connect();
+  //   await client.connect();
 
-    const res = await client.query(`SELECT datname FROM pg_catalog.pg_database WHERE datname = '${process.env.DB_DATABASE}'`);
+  //   const res = await client.query(`SELECT datname FROM pg_catalog.pg_database WHERE datname = '${process.env.DB_DATABASE}'`);
 
-    if (res.rowCount === 0) {
-      console.log(`${process.env.DB_DATABASE} database not found, creating it.`);
-      await client.query(`CREATE DATABASE "${process.env.DB_DATABASE}";`);
-      console.log(`created database ${process.env.DB_DATABASE}.`);
-    } else {
-      console.log(`${process.env.DB_DATABASE} database already exists.`);
-    }
+  //   if (res.rowCount === 0) {
+  //     console.log(`${process.env.DB_DATABASE} database not found, creating it.`);
+  //     await client.query(`CREATE DATABASE "${process.env.DB_DATABASE}";`);
+  //     console.log(`created database ${process.env.DB_DATABASE}.`);
+  //   } else {
+  //     console.log(`${process.env.DB_DATABASE} database already exists.`);
+  //   }
 
-    await client.end();
+  //   await client.end();
 
-  }
+  // }
 
 //   sequelize.authenticate().then(() => {
 //     console.log("Success!");
